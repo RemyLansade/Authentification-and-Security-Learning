91f88582506c88311b68e46e865215b83a932452
RemyLansade <boblol77@live.fr>
2020-07-20 11:02:07 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const bodyParser = require('body-parser');
+const ejs        = require('ejs');
+const express    = require('express');
+
+const app = express();
+
+const home     = require('./routes/home');
+const login    = require('./routes/login');
+const register = require('./routes/register');
+
+require('dotenv').config()
+
+
+// Middleware
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+// Roots
+app.use('/', home);
+app.use('/login', login);
+app.use('/register', register);
+
+
+// Server port
+const port = process.env.APP_PORT !== undefined ? 3000 : process.env.APP_PORT;
+
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
