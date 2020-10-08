88034a3fc0d200fa133554a84dddd519a9ace6cf
RemyLansade <boblol77@live.fr>
2020-10-08 23:42:08 +0200

    bcrypt dependencie add

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const md5 = require('md5');
+// const md5 = require('md5');
+const bcrypt = require('bcrypt');
 
-const User = require('../database/model/userSchema');
+const User = require('../database/model/user.model');
 
 router.get('/', (req, res) => {
     res.render('login');
@@ -11,19 +12,22 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
-
-    User.findOne({email: username}, (err, result) => {
-        if(err) {
-            res.sendStatus(500);
-        } else {
-            if(result){
-                if(result.password === password){
-                    res.render('secrets');
-                } else {
-                    res.send("Identifiant incorrect");
-                }
+    const password = req.body.password;
+    User.findOne({email: username}, (err, user) => {
+        if(!err) {
+            if(user){
+                bcrypt.compare(password, user.password, (error, result) => {
+                    if(result){
+                        res.render('secrets');
+                    } else {
+                        res.send("Identifiants incorrects")
+                    }
+                });
+            } else {
+                res.sendStatus(404);
             }
+        } else {
+            res.sendStatus(500);
         }
     });
 });
--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const md5 = require('md5');
+// const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-const User = require('../database/model/userSchema');
+const User = require('../database/model/user.model');
 
 
 router.get('/', (req, res) => {
@@ -12,22 +14,26 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
-
-    const newUser = new User({
-        email: username,
-        password: password
-    });
-
-    newUser.save((err) => {
-        if(err) {
-            console.log(err);
-            res.sendStatus(500);
+    const password = req.body.password;
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        if(!err){
+            const newUser = new User({
+                email: username,
+                password: hash
+            });
+        
+            newUser.save((err) => {
+                if(err) {
+                    console.log(err);
+                    res.sendStatus(500);
+                } else {
+                    res.render('secrets');
+                }
+            });
         } else {
-            res.render('secrets');
+            res.sendStatus(400);
         }
-    });
-    
+    });    
 });
 
 module.exports = router;
